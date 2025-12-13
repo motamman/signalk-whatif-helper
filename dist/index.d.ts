bce2253b7dba9793630f28134280091fcea6d4d9
Maurice Tamman <maurice@tamman.org>
2025-12-13 12:47:00 -0500

    Remove deprecated TypeScript definition files and source maps from the dist directory

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * SignalK What-If Helper Plugin
- *
- * A webapp for browsing, modifying, and creating SignalK paths
- * for testing and simulation purposes.
- */
-export {};
-//# sourceMappingURL=index.d.ts.map
--- dist/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * SignalK What-If Helper Type Definitions
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
